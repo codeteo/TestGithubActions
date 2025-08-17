38194e4c0d88e958e3ca579e560436d6e539fe8a
Theo Ioakeimidis <ramones.teo@gmail.com>
2025-08-17 22:26:04 +0300

    Adds buildLogic (#4)
    * Adds buildLogic
    
    * fixes

---
+++ build-logic/conventions/build.gradle.kts
@@ -0,0 +1,24 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    `kotlin-dsl`
+}
+
+group = "com.theo.buildlogic"
+
+// Configure the build-logic plugins to target JDK 17
+// This matches the JDK used to build the project, and is not related to what is running on device.
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_17
+    }
+}
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+}
